1c97c3528861d269da7f592f084e4aaaa21e9ce4
_hoo! <31012876+HandierChan@users.noreply.github.com>
2022-06-13 17:45:27 +0800

    Add files via upload

--- nuke/TThunderMenu/menu.py
+++ nuke/TThunderMenu/menu.py
@@ -1,32 +1,34 @@
 import os
 
-import KnobDefaults
 
 
 # load this Folder Path
-CurrentFolder = os.path.dirname(__file__)
-IconsFolder = "%s/icons" % CurrentFolder
-PresetsFolder = "%s/Presets" % CurrentFolder
-PresetsNetworkFolder = "%s/Presets/Network" % CurrentFolder
+tthMenuCurrentFolder = os.path.dirname(__file__)
+tthMenuIconsFolder = "%s/icons" % tthMenuCurrentFolder
+tthMenuPresetsFolder = "%s/Presets" % tthMenuCurrentFolder
+presetsNetworkFolder = "%s/Presets/Network" % tthMenuCurrentFolder
 
 
 # Read Node Parameter
 def unload_read():
     sels=nuke.selectedNodes()
     if len(sels):
-        [i.knob('localizationPolicy').setValue("formAutoLocalizePath") for i in sels]
+        [i.knob('localizationPolicy').setValue("formAutoLocalizePath") for i in sels if 'localizationPolicy' in i.knobs()]
     else: 
-        [i.knob('localizationPolicy').setValue("formAutoLocalizePath") for i in nuke.allNodes('Read')]
+        [i.knob('localizationPolicy').setValue("formAutoLocalizePath") for i in nuke.allNodes('Read') if 'localizationPolicy' in i.knobs()]
 def load_read():
     sels=nuke.selectedNodes()
     if len(sels):
-        [i.knob('localizationPolicy').setValue("on") for i in sels]
+        [i.knob('localizationPolicy').setValue("on") for i in sels if 'localizationPolicy' in i.knobs()]
     else: 
-        [i.knob('localizationPolicy').setValue("on") for i in nuke.allNodes('Read')]
+        [i.knob('localizationPolicy').setValue("on") for i in nuke.allNodes() if 'localizationPolicy' in i.knobs()]
 def reload_read():
-    for n in nuke.allNodes('Read'):
-        n.knob('updateLocalization').execute()
-        n.knob('reload').execute()
+    for i in nuke.allNodes():
+        if 'updateLocalization' in i.knobs():
+            i.knob('updateLocalization').execute()
+        if 'reload' in i.knobs():
+            i.knob('reload').execute()
+
 
 
 
@@ -71,29 +73,15 @@ def CreatePresetsMenu(origPath, menuObject):
         if os.path.isfile(FileFullPath):
             if (os.path.splitext(FileFullPath)[1] == '.nk'):
                 FileName = os.path.splitext(file)[0]
-                menuObject.addCommand(FileName, 'nuke.nodePaste(\'' + FileFullPath + '\')', icon='%s/%s.png'%(IconsFolder,FileName))
+                menuObject.addCommand(FileName, 'nuke.nodePaste(\'' + FileFullPath + '\')', icon='%s/%s.png'%(tthMenuIconsFolder,FileName))
 
 
-'''
-# Build Toolbar Menu, "Reveal In Explorer"
-def RevealInExplorer():
-    node = nuke.selectedNode()
-    if node.knob("file") is not None:
-    #if node.Class()=="Read" or node.Class()=="Write":
-        fullpath = node['file'].value()
-        path = os.path.split(fullpath)[0]
-        path = os.path.normpath(path)
-        cmd = 'explorer "%s"' % path
-        os.system(cmd)
-RevealMenu=nuke.toolbar('Nodes')
-RevealMenu.addCommand('Reveal In Explorer', RevealInExplorer, icon='%s/FileExplorer.png' % IconsFolder)
-'''
 
 
 #######################(Start) Build TTH Menu
 TThunderMenu = nuke.menu('Nuke').addMenu('TTH')
 
-Playback = nuke.menu('Nuke').addMenu('&TTH/Playback', icon='%s/Playback.png'%IconsFolder)
+Playback = nuke.menu('Nuke').addMenu('&TTH/Playback', icon='%s/Playback.png'%tthMenuIconsFolder)
 Playback.addCommand('FirstFrame', lambda: nuke.activeViewer().frameControl(-6), '^Up')
 Playback.addCommand('PreviousKeyframe', lambda: nuke.activeViewer().frameControl(-4), '^Left')
 Playback.addCommand('Pause', lambda: nuke.activeViewer().frameControl(0), 'Down')
@@ -102,27 +90,31 @@ Playback.addCommand('NextKeyframe', lambda: nuke.activeViewer().frameControl(4),
 Playback.addCommand('LastFrame', lambda: nuke.activeViewer().frameControl(6), '^Down')
 
 TThunderMenu.addSeparator()
-monitor = nuke.menu('Nuke').addMenu('&TTH/Performance Monitor', icon='%s/Monitor.png'%IconsFolder)
+monitor = nuke.menu('Nuke').addMenu('&TTH/Performance Monitor', icon='%s/Monitor.png'%tthMenuIconsFolder)
 monitor.addCommand('Start', lambda: nuke.startPerformanceTimers(), '', icon='')
 monitor.addCommand('Stop', lambda: nuke.stopPerformanceTimers(), '', icon='')
 
 TThunderMenu.addSeparator()
-Localization = nuke.menu('Nuke').addMenu('&TTH/Localization', icon='%s/Localization.png'%IconsFolder)
-Localization.addCommand('Unload (Read Node)', unload_read, 'F9', icon='%s/Write.png'%IconsFolder)
-Localization.addCommand('Load (Read Node)', load_read, '+F9', icon='%s/Read.png'%IconsFolder)
-Localization.addCommand('Reload (Read Node)', reload_read, '^F9', icon='%s/DeepRecolor.png'%IconsFolder)
+Localization = nuke.menu('Nuke').addMenu('&TTH/Localization', icon='%s/Localization.png'%tthMenuIconsFolder)
+Localization.addCommand('Unload (Read Node)', unload_read, 'F9', icon='%s/Write.png'%tthMenuIconsFolder)
+Localization.addCommand('Load (Read Node)', load_read, '+F9', icon='%s/Read.png'%tthMenuIconsFolder)
+Localization.addCommand('Reload (Read Node)', reload_read, '^F9', icon='%s/DeepRecolor.png'%tthMenuIconsFolder)
 
 TThunderMenu.addSeparator()
-LockNodeMenu = nuke.menu('Nuke').addMenu('&TTH/Lock Node', icon='%s/MarkerRemoval.png'%IconsFolder)
+LockNodeMenu = nuke.menu('Nuke').addMenu('&TTH/Lock Node', icon='%s/MarkerRemoval.png'%tthMenuIconsFolder)
 LockNodeMenu.addCommand('Lock', 'LockNode(True)', '')
 LockNodeMenu.addCommand('Unlock', 'LockNode(False)', '')
 
 TThunderMenu.addSeparator()
-PresetsMenu = nuke.menu('Nuke').addMenu('&TTH/Presets', icon='%s/NukeApp32.png'%IconsFolder)
-PresetsNetworkMenu = nuke.menu('Nuke').addMenu('&TTH/Presets/Network', icon='%s/NukeApp32.png'%IconsFolder)
-CreatePresetsMenu(PresetsNetworkFolder, PresetsNetworkMenu)
-CreatePresetsMenu(PresetsFolder, PresetsMenu)
+PresetsNetworkMenu = nuke.menu('Nuke').addMenu('&TTH/Presets/Network', icon='%s/NukeApp32.png'%tthMenuIconsFolder)
+PresetsMenu = nuke.menu('Nuke').addMenu('&TTH/Presets', icon='%s/NukeApp32.png'%tthMenuIconsFolder)
+CreatePresetsMenu(presetsNetworkFolder, PresetsNetworkMenu)
+CreatePresetsMenu(tthMenuPresetsFolder, PresetsMenu)
 
+# Open Preset Directory
+def openPresetDirectory():
+    os.startfile(os.path.normpath(tthMenuPresetsFolder))
+PresetsMenu.addCommand('Open Preset Directory', openPresetDirectory)
 
 #######################(End) Build TTH Menu
 
